18326a7f18cffdb419ab3a57ed3e72ac1688ea0a
Inge Grelland <inge@crazy4life.no>
2023-04-25 14:26:20 +0200

    Maybe fixed backend for elsys day

--- server/server.py
+++ server/server.py
@@ -1,17 +1,18 @@
+from cgi import parse_header, parse_multipart
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import time
 
 
 # Server settings
-hostname = '192.168.116.202'
+hostname = 'localhost'
 serverPort = 8000
-dataName = "video1"
+dataName = "video4"
 
 
 # Hacky way to expose stuff publicly for the request handler
 data = {}
-with open("server/tekst.json", "r") as file:
+with open("nettside/tekst.json", "r") as file:
     data = json.loads("".join(file.readlines()))[dataName]
 screenTime = int(time.time() * 1000) # Time expressed in ms
 curIndex = 0
@@ -19,6 +20,19 @@ curIndex = 0
 
 # Server
 class MyServer(BaseHTTPRequestHandler):
+    def do_POST(self):
+        global dataName, screenTime, curIndex
+        if self.path() != "/setvideo":
+            self.send_error(400, message="bad POST", explain="POST request was sent on a path that is not supported")
+            self.log_error(code=400)
+
+        self.send_response(200)
+        postvars = self.parse_POST()
+        dataName = postvars[id]
+        screenTime = int(time.time() * 1000) - postvars[time]
+        curIndex = 0
+
+
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -31,6 +45,15 @@ class MyServer(BaseHTTPRequestHandler):
         else:
             self.getData(int(self.path[1:]))
     
+    
+    def parse_POST(self):
+        ctype, pdict = parse_header(self.headers['content-type'])
+        if ctype == 'multipart/form-data':
+            postvars = parse_multipart(self.rfile, pdict)
+        else:
+            postvars = {}
+        return postvars
+
 
     def getCurrentIndex(self):
         global screenTime, curIndex
